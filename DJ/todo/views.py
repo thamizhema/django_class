1a89e05e4136739bbe81e8329ffc59297069c3c1
ijass <jass07rtr@gmail.com>
2023-01-04 12:45:04 +0530

    django class

---
+++ DJ/todo/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+products = ["milk", 'sugar', 'ghee', 'butter']
+userInfo = {'name': "python", 'email': "python@gmail.com"}
+
+
+def home(req):
+    data = {
+        'username': "JavaScript",
+        'age': 20,
+        "products": products,
+        'userInfo': userInfo
+    }
+    return render(req, 'home.html', data)
+
+
+def login(req):
+    return render(req, 'login.html')
+
+
+def signup(req):
+    return render(req, 'signup.html')
